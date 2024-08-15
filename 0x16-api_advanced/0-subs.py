a989267cb926bd21a55f3eb2b38cf2ccfd8ee13f
Auspicious-1ife <melissagushi50@gmail.com>
2024-08-15 12:58:00 +0100

    updated task 0

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,22 +1,32 @@
 #!/usr/bin/python3
-"""Function to query subscribers on a given Reddit subreddit."""
+"""
+0-subs - Function to query subscribers on a given Reddit subreddit.
+"""
+
 import requests
 
 
 def number_of_subscribers(subreddit):
-    """Return the total number of subscribers on a given subreddit."""
+    """
+    Queries the Reddit API to get the number of subscribers,
+    for a subreddit.
+
+    Args:
+        subreddit (str): The name of the subreddit.
+
+    Returns:
+        int: The number of subscribers if the subreddit is valid, otherwise 0.
+    """
+    url = f'https://www.reddit.com/r/{subreddit}/about.json'
+    headers = {
+        'User-Agent': 'my-reddit-api-client'
+    }
     try:
-        url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
-        headers = {
-            "User-Agent": "linux:0x16.api.advanced:v1.0.0\
-            (by /u/Large_Alternative_30)"
-        }
         response = requests.get(url, headers=headers, allow_redirects=False)
-        if response.status_code == 404:
+        if response.status_code == 200:
+            data = response.json()
+            return data['data']['subscribers']
+        else:
             return 0
-        results = response.json().get("data")
-        return results.get("subscribers")
-    except (Exception):
-        print('Not Found')
-        return (0)
-    
+    except Exception:
+        return 0
