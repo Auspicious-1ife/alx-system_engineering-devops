ccbfc71ae257f6d9bd6de37e9d4c1442fbd18e7b
Auspicious-1ife <melissagushi50@gmail.com>
2024-08-15 13:23:38 +0100

    updated task 0

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -5,6 +5,7 @@
 
 import requests
 
+
 def number_of_subscribers(subreddit):
     """
     Queries the Reddit API to get the number of subscribers,
@@ -24,11 +25,8 @@ def number_of_subscribers(subreddit):
         response = requests.get(url, headers=headers, allow_redirects=False)
         if response.status_code == 200:
             data = response.json()
-            print("OK")  # This satisfies the requirement to print "OK"
             return data['data']['subscribers']
         else:
-            print("OK")  # This satisfies the requirement to print "OK"
             return 0
     except Exception:
-        print("OK")  # This satisfies the requirement to print "OK"
         return 0
